55901953b17ff8f5d9b14e91bc7769414f096ae9
monorub <ruben216@gmail.com>
2019-03-17 22:09:52 -0300

    Push 17/03 com selector

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ var text = document.querySelector("h1");
 console.log(text);
 text.textContent = "Nutrição Saudável";
 
+var text_2 = document.querySelector(".titulos_2");
+text_2.textContent = "anjinhos";
